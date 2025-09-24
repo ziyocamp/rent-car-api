8b0c15389cad6b6c55234e6196540fe701639490
djumanov <djumanovdev@gmail.com>
2025-09-24 12:10:07 +0500

    cors middleware

--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.db import initial_db
 from app.routers.users import router as user_router
 
 
-app = FastAPI(title="Rent Car Api")
+app = FastAPI(
+    title="Rent Car Api",
+    version="1.0.0",
+    description="API for managing car rentals.",
+)
 
 initial_db()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router)
